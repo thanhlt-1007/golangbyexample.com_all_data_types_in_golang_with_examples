4a39b9c20bc04052dc697ea16d79b4c23328c77f
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-11-19 11:46:55 +0700

    Update main.go Add struct

--- main.go
+++ main.go
@@ -135,10 +135,37 @@ func main() {
     // fmt.Println("Output of NEGATION operation on false value: %t\n", negationOperation)
 
     // array
-    sample := [3]string{"a", "b", "c"}
-    print(sample)
+    // sample := [3]string{"a", "b", "c"}
+    // print(sample)
+
+    // struct
+    // Initialize a struct without named fields
+    employee1 := employee{"John", 21, 1000}
+    fmt.Println(employee1)
+
+    // Initialize a struct with named fields
+    employee2 := employee{
+        name: "Sam",
+        age: 22,
+        salary: 1100,
+    }
+    fmt.Println(employee2)
+
+    // Initialize only some fields.
+    // Other values are initialized to default zero value of that type
+    employee3 := employee{
+        name: "Tina",
+        age: 24,
+    }
+    fmt.Println(employee3)
 }
 
 func print(sample [3]string) {
     fmt.Println(sample)
+}
+
+type employee struct {
+    name string
+    age int
+    salary float64
 }
