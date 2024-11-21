dce8c92f8886eee7826ce090ef31448981a9aef6
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-11-21 09:33:51 +0700

    Update main.go add slice

--- main.go
+++ main.go
@@ -140,24 +140,42 @@ func main() {
 
     // struct
     // Initialize a struct without named fields
-    employee1 := employee{"John", 21, 1000}
-    fmt.Println(employee1)
+    // employee1 := employee{"John", 21, 1000}
+    // fmt.Println(employee1)
 
-    // Initialize a struct with named fields
-    employee2 := employee{
-        name: "Sam",
-        age: 22,
-        salary: 1100,
-    }
-    fmt.Println(employee2)
+    // // Initialize a struct with named fields
+    // employee2 := employee{
+    //     name: "Sam",
+    //     age: 22,
+    //     salary: 1100,
+    // }
+    // fmt.Println(employee2)
 
-    // Initialize only some fields.
-    // Other values are initialized to default zero value of that type
-    employee3 := employee{
-        name: "Tina",
-        age: 24,
+    // // Initialize only some fields.
+    // // Other values are initialized to default zero value of that type
+    // employee3 := employee{
+    //     name: "Tina",
+    //     age: 24,
+    // }
+    // fmt.Println(employee3)
+
+    // slices
+    // Declare a slice using make
+    s := make([]string, 2, 3)
+    fmt.Println(s)
+
+    // Direct initialization
+    p := []string{"a", "b", "c"}
+    fmt.Println(p)
+
+    // Append function
+    p = append(p, "d")
+    fmt.Println(p)
+
+    // Iterate over a slice
+    for _, val := range p {
+        fmt.Println(val)
     }
-    fmt.Println(employee3)
 }
 
 func print(sample [3]string) {
